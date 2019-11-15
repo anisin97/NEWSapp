764919f175c1444978792c9eee55693cb9d92ac5
theprover97 <theprover97@gmail.com>
2019-11-15 21:45:06 +0530

    Create api_sources.js

---
+++ api_sources.js
@@ -0,0 +1,17 @@
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI('API_KEY');
+// To query sources
+// All options are optional
+newsapi.v2.sources({
+  category: 'technology',
+  language: 'en',
+  country: 'us'
+}).then(response => {
+  console.log(response);
+  /*
+    {
+      status: "ok",
+      sources: [...]
+    }
+  */
+});
