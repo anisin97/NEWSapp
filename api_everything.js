2c47c48726fead8b7c6f1c41c1f425b7fa8f620d
theprover97 <theprover97@gmail.com>
2019-11-15 21:42:32 +0530

    Create api_everything.js

---
+++ api_everything.js
@@ -0,0 +1,22 @@
+const NewsAPI = require('newsapi');
+const newsapi = new NewsAPI('API_KEY');
+// To query /v2/everything
+// You must include at least one q, source, or domain
+newsapi.v2.everything({
+  q: 'bitcoin',
+  sources: 'bbc-news,the-verge',
+  domains: 'bbc.co.uk, techcrunch.com',
+  from: '2017-12-01',
+  to: '2017-12-12',
+  language: 'en',
+  sortBy: 'relevancy',
+  page: 2
+}).then(response => {
+  console.log(response);
+  /*
+    {
+      status: "ok",
+      articles: [...]
+    }
+  */
+});
